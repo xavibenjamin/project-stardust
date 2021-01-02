25d4d579a8f89e334822c581636953aba41559fd
Timothy B. Smith <tim@smithtimmytim.com>
2021-01-02 00:51:48 -0800

    add markup for site id

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?
 get_header();
+?>
 
+<div class="wrapper">
+  <div class="[ site-id ] [ h-card ]">
+    <div class="site-id__image">
+      <img class="u-photo" src="<?php echo get_template_directory_uri(); ?>/assets/img/tim-smith-avatar.jpg" alt="Tim Smith Avatar Illustration">
+    </div>
+    <div class="site-id__info">
+      <h1 class="site-name">
+        <a class="p-name u-url" rel="me" href="/">
+          Timothy Smith
+        </a>
+        <span class="site-pronouns">he/him</span>
+      </h1>
+      <p class="site-bio">Geek, coffee snob, and maker of stuff.</p>
+    </div>
+  </div>
+</div>
+
+
+<?php
 get_footer();
 ?>
