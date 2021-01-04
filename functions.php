cefd7d2d9d376d59566eb0f4618d87cb01d9c7e2
Timothy B. Smith <tim@smithtimmytim.com>
2021-01-03 20:30:14 -0800

    add theme support for feeds and titles

--- functions.php
+++ functions.php
@@ -4,10 +4,10 @@ function stardust_scripts() {
   wp_enqueue_script( 'stardust-scripts', get_stylesheet_directory_uri() . '/dist/frontend-bundle.js', [], '', true );
   wp_enqueue_style( 'stardust-styles', get_stylesheet_directory_uri() . '/dist/frontend.css' );
 }
-
 add_action( 'wp_enqueue_scripts', 'stardust_scripts');
 
 function stardust_setup() {
+
   // Add post-formats support
   add_theme_support(
     'post-formats',
@@ -25,6 +25,11 @@ function stardust_setup() {
   
   // Add support for responsive embedded content.
   add_theme_support( 'responsive-embeds' );
-}
 
+  // Add support for Wordpress generated titles
+  add_theme_support( 'title-tag' );
+
+  // Automatic feed
+  add_theme_support( 'automatic-feed-links' );
+}
 add_action( 'after_setup_theme', 'stardust_setup' );
