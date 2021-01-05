12bca9d8c95c6b4727dc58f0149619eaa53e90b8
Timothy B. Smith <tim@smithtimmytim.com>
2021-01-04 22:23:14 -0800

    add twemoji support

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
 <!--WP Generated Footer -->
 <?php wp_footer(); ?>
 <!--End WP Generated Footer -->
+<script aync src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js" crossorigin="anonymous"></script>
+<script>
+  twemoji.parse(document.body, {
+    folder:'svg',
+    ext:'.svg'
+  });
+</script>
 </body>
 </html> 
