be52b6c4bb700cfe70668691a1ade41abc632fb8
Timothy B. Smith <tim@smithtimmytim.com>
2021-01-09 17:07:35 -0800

    remove image partial

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ get_header();
       <?php if (have_posts()) : while (have_posts()) : the_post();
 
         if ( has_post_format( 'status') || has_post_format( 'image') ) {
-          get_template_part( 'template-parts/content/format', get_post_format() );
+          get_template_part( 'template-parts/content/format-status' );
         } else {
           get_template_part( 'template-parts/content/format-standard' );
         }
