e35a88789afc583f125c8bbe2acc1b1e6802a6da
Timothy B. Smith <tim@smithtimmytim.com>
2021-01-30 14:34:46 -0800

    filter archive title

--- functions.php
+++ functions.php
@@ -106,8 +106,28 @@ function sd_custom_excerpt_length( $length ) {
 }
 add_filter( 'excerpt_length', 'sd_custom_excerpt_length', 999 );
 
-// 
+// Modify Excerpt Ending
 function sd_excerpt_more( $more ) {
     return '…';
 }
 add_filter( 'excerpt_more', 'sd_excerpt_more' );
+
+//
+function sd_archive_title( $title ) {
+    if ( is_category() ) {
+        $title = single_cat_title( '', false );
+    } elseif ( is_tag() ) {
+        $title = single_tag_title( '', false );
+    } elseif ( is_author() ) {
+        $title = '<span class="vcard">' . get_the_author() . '</span>';
+    } elseif ( is_post_type_archive() ) {
+        $title = post_type_archive_title( '', false );
+    } elseif ( is_tax() ) {
+        $title = single_term_title( '', false );
+    } elseif ( is_month() ) {
+        $title  = get_the_date( _x( 'F Y', 'monthly archives date format' ) );
+    }
+  
+    return $title;
+}
+add_filter( 'get_the_archive_title', 'sd_archive_title' );
