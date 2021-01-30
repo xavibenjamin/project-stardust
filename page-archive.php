6bdf3408b0f4886fafe5a5f6dcf536b61efab6e2
Timothy B. Smith <tim@smithtimmytim.com>
2021-01-30 13:59:46 -0800

    update archive page

--- page-archive.php
+++ page-archive.php
@@ -8,27 +8,24 @@
 
 <main tabindex="-1" id="main-content" class="site-content">
   <div class="wrapper">
-
-    <div class="archive-list">
-
-      <?php
-
-      $query = new WP_Query( array( 'posts_per_page' => -1 ) );
-      
-      if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-
-       <article class="archive-list__item">
-        <span class="archive-list__item-title"><?php the_title(); ?></span>
-        <span class="archive-list__item-excerpt"><?php echo get_the_excerpt(); ?></span>&nbsp;<a href="<?php the_permalink(); ?>" class="archive-list__item-link"><time class="dt-published"><?php echo get_the_date('M j, Y'); ?></time></a>
-      </article>
-      
-      <?php
-      endwhile; endif;
-
-      /* Restore original Post Data */
-      wp_reset_postdata(); ?>
-
+  
+    <div class="taxonomy-list">
+      <h2 class="taxonomy-list__title">Browse by Subject</h2>
+      <ul class="taxonomy-list__list">
+			 <?php wp_list_categories( array(
+        'exclude'  => 1,
+        'title_li' => ''
+    ) ); ?>
+      </ul>
     </div>
+
+    <div class="taxonomy-list">
+      <h2 class="taxonomy-list__title">Browse by Year</h2>
+      <ul class="taxonomy-list__list">
+			 <?php wp_get_archives('type=yearly'); ?>
+      </ul>
+    </div>
+
   </div>
 </main>
 
