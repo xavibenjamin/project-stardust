a8e5f06074661346d2be014119d870c22cd72f69
Timothy B. Smith <tim@smithtimmytim.com>
2021-01-31 22:35:11 -0800

    update theme

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" data-theme="halcyon">
+<html lang="en" data-theme="github-dark">
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
