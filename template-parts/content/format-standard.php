545894eeacff48a5d08d62d54c7bca85ca707647
Timothy B. Smith <tim@smithtimmytim.com>
2021-02-03 22:37:19 -0800

    add rating and syndication link

--- template-parts/content/format-standard.php
+++ template-parts/content/format-standard.php
@@ -10,6 +10,14 @@
     <div class="entry__meta">
       <a href="<?php the_permalink(); ?>" class="entry__date u-url"><time class="dt-published" datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date('D, M jS, Y'); ?></time></a>
     </div>
+
+    <?php 
+      if ( get_post_meta( get_the_ID(), 'rating', true ) ) {
+        get_template_part(
+          'template-parts/svg/svg_rating'
+        );
+      } 
+      ?>
   </header>
 
   <?php if (has_post_thumbnail()) : ?>
@@ -27,6 +35,21 @@
 
   <?php if (is_singular('post')) : ?>
     <footer class="entry__footer">
+      <?php if ( get_post_meta( get_the_ID(), 'letterboxd_url', true ) ) : ?>
+        <span class="entry__syndicated">
+          <?php esc_html_e( 'Syndicated to', 'stardust' );?>
+          <a href="<?php echo esc_url( get_post_meta( get_the_ID(), 'letterboxd_url', true ) ); ?>" target="_blank" rel="noreferrer noopener">
+            <?php esc_html_e( 'Letterboxd', 'stardust' );?>
+            <?php
+              get_template_part(
+                'template-parts/svg/icon',
+                'share'
+              );
+              ?>
+          </a>
+        </span>
+
+      <?php endif; ?>
       <?php if (comments_open() || get_comments_number() > 0) : ?>
         <div id="post-discussion" class="entry-comments">
           <div class="entry-comments__comments">
