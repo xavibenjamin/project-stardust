f5b12c35bc17d70d6f5e89bccf9b996e805e1aab
Timothy B. Smith <tim@smithtimmytim.com>
2021-02-03 22:37:41 -0800

    add rating to excerpt

--- template-parts/content/excerpt.php
+++ template-parts/content/excerpt.php
@@ -1,4 +1,3 @@
 <article class="archive-list__item">
-  <?php if ( get_the_title() ) : ?><span class="archive-list__item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span><?php endif; ?>
-  <span class="archive-list__item-excerpt"><?php echo get_the_excerpt(); ?></span>&nbsp;<a href="<?php the_permalink(); ?>" class="archive-list__item-link"><time class="dt-published"><?php echo get_the_date('M j, Y'); ?></time></a>
+  <?php if ( get_the_title() ) : ?><span class="archive-list__item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span><?php endif; ?><?php if ( get_post_meta( get_the_ID(), 'rating', true ) ) { get_template_part( 'template-parts/svg/svg_rating' ); } ?> <span class="archive-list__item-excerpt"><?php echo get_the_excerpt(); ?></span>&nbsp;<a href="<?php the_permalink(); ?>" class="archive-list__item-link"><time class="dt-published"><?php echo get_the_date('M j, Y'); ?></time></a>
 </article>
