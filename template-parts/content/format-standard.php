3063de0534e7796df0787e8ae0873e6721d7ee62
Timothy B. Smith <tim@smithtimmytim.com>
2021-02-03 18:56:38 -0800

    update datetime

--- template-parts/content/format-standard.php
+++ template-parts/content/format-standard.php
@@ -2,20 +2,20 @@
   <header class="entry__header">
     <div class="entry__taxonomy"><?php the_category(' '); ?></div>
     <h1 class="entry__title">
-      <a href="<?php the_permalink(); ?>"><?php echo theme_widont( get_the_title() ); ?></a>
+      <a href="<?php the_permalink(); ?>"><?php echo theme_widont(get_the_title()); ?></a>
     </h1>
-    <?php if ( has_excerpt() ) : ?>
-      <h2 class="entry__subtitle"><?php echo theme_widont( get_the_excerpt() ); ?></h2>
+    <?php if (has_excerpt()) : ?>
+      <h2 class="entry__subtitle"><?php echo theme_widont(get_the_excerpt()); ?></h2>
     <?php endif; ?>
     <div class="entry__meta">
-      <a href="<?php the_permalink(); ?>" class="entry__date u-url"><time class="dt-published"><?php echo get_the_date('D, M jS, Y'); ?></time></a>
+      <a href="<?php the_permalink(); ?>" class="entry__date u-url"><time class="dt-published" datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date('D, M jS, Y'); ?></time></a>
     </div>
   </header>
 
-  <?php if ( has_post_thumbnail() ) : ?>
+  <?php if (has_post_thumbnail()) : ?>
     <figure class="entry__featured-image">
-      <?php the_post_thumbnail( 'medium_large' ); ?>
-      <?php if ( get_the_post_thumbnail_caption() ) : ?>
+      <?php the_post_thumbnail('medium_large'); ?>
+      <?php if (get_the_post_thumbnail_caption()) : ?>
         <figcaption class="entry__featured-image-caption"><?php the_post_thumbnail_caption(); ?></figcaption>
       <?php endif; ?>
     </figure>
@@ -25,17 +25,17 @@
     <?php the_content(); ?>
   </div>
 
-  <?php if ( is_singular('post') ) : ?>
-  <footer class="entry__footer">
-    <?php if ( comments_open() || get_comments_number() > 0 ) : ?>
-      <div id="post-discussion" class="entry-comments">        
-        <div class="entry-comments__comments">
-          <?php comments_template(); ?>
+  <?php if (is_singular('post')) : ?>
+    <footer class="entry__footer">
+      <?php if (comments_open() || get_comments_number() > 0) : ?>
+        <div id="post-discussion" class="entry-comments">
+          <div class="entry-comments__comments">
+            <?php comments_template(); ?>
+          </div>
+
         </div>
-        
-      </div>
-    <?php endif; ?>
-  </footer>
+      <?php endif; ?>
+    </footer>
   <?php endif; ?>
 
 </article>
