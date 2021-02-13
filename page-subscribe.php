bdc20b7f317dce0e2b099405228e10a6263de6f6
Timothy B. Smith <tim@smithtimmytim.com>
2021-02-12 22:48:00 -0800

    add feed url to subscribe page

--- page-subscribe.php
+++ page-subscribe.php
@@ -9,6 +9,7 @@ get_header(); ?>
     <div class="site-newsletter">
       <h1 class="site-newsletter__title"><?php the_title(); ?></h1>
       <?php the_content(); ?>
+      <small>If email isn't your thing, <a href="<?php bloginfo('rss2_url'); ?>">grab the <span class="caps">RSS</span> feed</a>.</small>
     </div>
   </div>
 </main>
