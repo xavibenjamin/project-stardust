12206d847d1ed7582dc1c92e7a23bb60e0e95600
Timothy B. Smith <tim@smithtimmytim.com>
2021-02-18 22:41:06 -0800

    add popular tags

--- page-archive.php
+++ page-archive.php
@@ -18,6 +18,28 @@
     ) ); ?>
       </ul>
     </div>
+    
+    <div class="taxonomy-list">
+      <h2 class="taxonomy-list__title">Browse by Popular Tags</h2>
+      <?php
+      $tags = get_tags(
+        array(
+          'orderby' => 'count',
+          'order'   => 'DESC',
+          'number'  => 20
+        )
+      );
+      $html = '<ul class="taxonomy-list__list">';
+      foreach ( $tags as $tag ) {
+          $tag_link = get_tag_link( $tag->term_id );
+                  
+          $html .= "<li><a href='{$tag_link}' title='Tag: {$tag->name}'>";
+          $html .= "{$tag->name}</a></li>";
+      }
+      $html .= '</ul>';
+      echo $html;
+      ?>
+    </div>
 
     <div class="taxonomy-list">
       <h2 class="taxonomy-list__title">Browse by Month</h2>
