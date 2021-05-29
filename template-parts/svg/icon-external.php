185a905581447edacb97ca0322786cfa052d2892
Timothy B. Smith <tim@smithtimmytim.com>
2021-05-28 18:18:32 -0700

    add icon external just in case I need it later

---
+++ template-parts/svg/icon-external.php
@@ -0,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" stroke="#ba90f6" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"><path d="M15 3h6v6M21 3L11 13M19 18v1a2 2 0 01-2 2H5a2 2 0 01-2-2V7c0-1.1.9-2 2-2h1"/></g></svg>
