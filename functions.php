18196ef4fe562d26d04b0cc920c0335d6c7ef229
Timothy B. Smith <tim@smithtimmytim.com>
2021-09-26 18:07:48 -0700

    version bump

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 // Useful global constants.
-define( 'SD_VERSION', '2021.26' );
+define( 'SD_VERSION', '2021.27' );
 define( 'SD_TEMPLATE_URL', get_template_directory_uri() );
 define( 'SD_PATH', get_template_directory() . '/' );
 define( 'SD_INC', SD_PATH . 'inc/' );
