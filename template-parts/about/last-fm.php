dbd3c907eaf3fb5c7e2f47c0a33b24b5b0bbf645
Timothy B. Smith <tim@smithtimmytim.com>
2021-09-26 17:08:39 -0700

    update last fm partial markup

--- template-parts/about/last-fm.php
+++ template-parts/about/last-fm.php
@@ -8,60 +8,58 @@
  */
 
   $tracks = $args['recenttracks']['track'] ?? false;
-  
 ?>
 
 <?php if ( ! empty( $tracks ) ) : ?>
-  <div class="recently-played">
-    <div class="wrapper wrapper--large">
-      <h2>Listening To</h2>
-      <p>I do some of my best work listening to music. Here's a list of tracks I've got playing.</p>
+<div class="recently-played">
 
-       <ul class="recently-played__list">
-        <?php 
-          foreach( $tracks as $track ) : 
-            [ 
-              'name' => $song_name, 
-              'url' => $song_url, 
-              'artist' => [
-                '#text' => $artist_name
-              ],
-              'image' => [
-                '3' => [
-                  '#text' => $cover_img
-                ]
-              ]
-            
-            ] = $track;
-          ?>
+  <h2 class="recently-played__title">
+    <?php esc_html_e( 'Recently Listened', 'stardust' ); ?>
+    <?php get_template_part( 'template-parts/svg/icon-music' ); ?>
+  </h2>
 
-          <li>
-            <article class="track">
-              <div class="track__media">
-                <img 
-                  src="<?php echo esc_attr( $cover_img ); ?>" 
-                  alt="Cover Art for <?php echo esc_attr( $song_name ); ?> by <?php echo esc_attr( $artist_name ); ?>">
-              </div>
-              <div class="track__info">
-                <h3 class="track__title">
-                  <?php echo esc_attr( $song_name ); ?>
-                </h3>
-                <a 
-                  class="track__link" 
-                  href="<?php echo esc_attr( $song_url ); ?>" target="_blank"
-                ></a>
-                <div class="track__artist">
-                  <span>
-                    <?php echo esc_attr( $artist_name ); ?>
-                  </span>
-                </div>
-              </div>
-            </article>
-          </li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
+  <ul class="recently-played__list">
+    <?php 
+      foreach( $tracks as $track ) : 
+        [ 
+          'name' => $song_name, 
+          'url' => $song_url, 
+          'artist' => [
+            '#text' => $artist_name
+          ],
+          'image' => [
+            '1' => [
+              '#text' => $cover_img
+            ]
+          ]
+        
+        ] = $track;
+      ?>
 
-  </div>
-
+      <li>
+        <article class="track">
+          <div class="track__media">
+            <img 
+              src="<?php echo esc_attr( $cover_img ); ?>" 
+              alt="Cover Art for <?php echo esc_attr( $song_name ); ?> by <?php echo esc_attr( $artist_name ); ?>">
+          </div>
+          <div class="track__info">
+            <h3 class="track__title">
+              <?php echo esc_attr( $song_name ); ?>
+            </h3>
+            <a 
+              class="track__link" 
+              href="<?php echo esc_attr( $song_url ); ?>" target="_blank"
+            ></a>
+            <div class="track__artist">
+              <span>
+                <?php echo esc_attr( $artist_name ); ?>
+              </span>
+            </div>
+          </div>
+        </article>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
 <?php endif; ?>
