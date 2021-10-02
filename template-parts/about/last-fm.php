4bafdd23686e35656ea0a26abd67cebf522c9a5a
Timothy B. Smith <tim@smithtimmytim.com>
2021-10-01 22:46:33 -0700

    refactor last fm function

--- template-parts/about/last-fm.php
+++ template-parts/about/last-fm.php
@@ -7,18 +7,20 @@
  * @package Stardust
  */
 
-$tracks = $args['recenttracks']['track'] ?? false;
+use function Stardust\DataLastFm\get_lastfm_data;
+
+$tracks = get_lastfm_data();
 ?>
 
 <?php if ( ! empty( $tracks ) ) : ?>
-<div class="recently-played">
+<div class="recently-played sidebar-section">
 
-	<h2 class="recently-played__title">
+	<h2 class="sidebar-section__title">
 		<?php esc_html_e( 'Recently Listened', 'stardust' ); ?>
 		<?php get_template_part( 'template-parts/svg/icon-music' ); ?>
 	</h2>
 
-	<ul class="recently-played__list">
+	<ul class="sidebar-section__list">
 		<?php
 		foreach ( $tracks as $track ) :
 			[
@@ -28,30 +30,29 @@ $tracks = $args['recenttracks']['track'] ?? false;
 					'#text' => $artist_name
 				],
 				'image' => [
-					'1' => [
+					'2' => [
 						'#text' => $cover_img
 					]
 				]
-
 			] = $track;
 			?>
 
 			<li>
-				<article class="track">
-					<div class="track__media">
+				<article class="card card--song">
+					<div class="card__media">
 						<img
 							src="<?php echo esc_attr( $cover_img ); ?>"
 							alt="Cover Art for <?php echo esc_attr( $song_name ); ?> by <?php echo esc_attr( $artist_name ); ?>">
 					</div>
-					<div class="track__info">
-						<h3 class="track__title">
-						<?php echo esc_attr( $song_name ); ?>
+					<div class="card__info">
+						<h3 class="card__title">
+							<?php echo esc_attr( $song_name ); ?>
 						</h3>
 						<a
-							class="track__link"
+							class="card__link"
 							href="<?php echo esc_attr( $song_url ); ?>" target="_blank"
 						></a>
-						<div class="track__artist">
+						<div class="card__meta">
 							<span>
 							<?php echo esc_attr( $artist_name ); ?>
 							</span>
