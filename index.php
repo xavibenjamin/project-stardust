f3ac283253954ec8922fb6745bc4c7ca9743c611
Timothy B. Smith <tim@smithtimmytim.com>
2021-10-24 02:05:17 -0700

    point to one template

--- index.php
+++ index.php
@@ -15,11 +15,9 @@ get_header();
 			<?php
 			if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-					if ( has_post_format( 'status' ) || has_post_format( 'image' ) ) {
-						get_template_part( 'template-parts/content/format-status' );
-					} else {
-						get_template_part( 'template-parts/content/format-standard' );
-					}
+					get_template_part(
+						'template-parts/content/content'
+					);
 
 			endwhile;
 endif;
