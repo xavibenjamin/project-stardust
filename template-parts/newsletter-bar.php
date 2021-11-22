951fd74035145193aac699b23923c6a18958713c
Timothy B. Smith <tim@smithtimmytim.com>
2021-11-22 00:05:19 -0800

    update newsletter bar background

--- template-parts/newsletter-bar.php
+++ template-parts/newsletter-bar.php
@@ -13,26 +13,31 @@ if ( is_active_sidebar( 'newsletter-bar-widget-area' ) ) : ?>
 	<div class="newsletter-bar__wave">
 		<?php get_template_part( 'template-parts/svg/svg', 'wave' ); ?>
 	</div>
-	<div class="newsletter-bar__inner wrapper wrapper--large">
-		<button
-			id="js-newsletter-bar-close"
-			class="newsletter-bar__close"
-		>
-			<?php get_template_part( 'template-parts/svg/icon', 'cancel' ); ?>
-		</button>
+	<div class="newsletter-bar__inner">
+		<div class="wrapper wrapper--large">
+			<div class="newsletter-bar__body">
+				<button
+					id="js-newsletter-bar-close"
+					class="newsletter-bar__close"
+				>
+					<?php get_template_part( 'template-parts/svg/icon', 'cancel' ); ?>
+				</button>
 
-		<div class="newsletter-bar__text">
-			<h3 class="newsletter-bar__title">
-				<?php echo esc_html( $title ); ?>
-			</h3>
-			<p class="newsletter-bar__description">
-				<?php echo esc_html( $description ); ?>
-			</p>
+				<div class="newsletter-bar__text">
+					<h3 class="newsletter-bar__title">
+						<?php echo esc_html( $title ); ?>
+					</h3>
+					<p class="newsletter-bar__description">
+						<?php echo esc_html( $description ); ?>
+					</p>
+				</div>
+
+				<div class="newsletter-bar__form">
+					<?php dynamic_sidebar( 'newsletter-bar-widget-area' ); ?>
+				</div>
+			</div>
 		</div>
 
-		<div class="newsletter-bar__form">
-			<?php dynamic_sidebar( 'newsletter-bar-widget-area' ); ?>
-		</div>
 	</div>
 </div>
 <?php endif; ?>
