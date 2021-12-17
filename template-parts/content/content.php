808640c94ddd5791822f27e7329bf1ee0452e186
Timothy B. Smith <tim@smithtimmytim.com>
2021-12-16 20:56:27 -0800

    add snippets stuff to template

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -12,8 +12,9 @@ $args   = [
 	'rating' => $rating,
 ];
 
-$title   = ! empty( get_the_title() ) ? widont( get_the_title() ) : '';
-$excerpt = has_excerpt() ? widont( get_the_excerpt() ) : '';
+$title     = ! empty( get_the_title() ) ? widont( get_the_title() ) : '';
+$excerpt   = has_excerpt() ? widont( get_the_excerpt() ) : '';
+$post_type = get_post_type( get_the_ID() );
 
 ?>
 
@@ -23,7 +24,11 @@ $excerpt = has_excerpt() ? widont( get_the_excerpt() ) : '';
 			<?php if ( has_category() ) : ?>
 				<div class="entry__taxonomy"><?php the_category( ' ' ); ?></div>
 			<?php endif; ?>
-			<h1 class="entry__title headline">
+			<h1 class="entry__title<?php echo 'sd_snippet' === $post_type ? ' entry__title--snippet' : ' headline'; ?>">
+			<?php if ( 'sd_snippet' === $post_type ) : ?>
+				<a class="title__parent" href="/snippets/">snippets</a>
+				<span class="title__separator">/</span>
+			<?php endif; ?>
 				<a href="<?php the_permalink(); ?>"><?php echo wp_kses_post( $title ); ?></a>
 			</h1>
 			<?php if ( has_excerpt() ) : ?>
@@ -62,6 +67,19 @@ $excerpt = has_excerpt() ? widont( get_the_excerpt() ) : '';
 	</div>
 
 	<footer class="entry__footer">
+		<?php if ( is_singular( 'sd_snippet' ) ) : ?>
+			<?php
+				get_template_part(
+					'template-parts/content/snippets/snippet-types'
+				);
+			?>
+
+			<div class="entry__meta">
+				<a href="<?php the_permalink(); ?>" class="entry__date u-url"><span class="entry__date-arrow">&rarr;</span>Last Updated on <time class="dt-published" datetime="<?php echo esc_attr( get_the_modified_date( 'c' ) ); ?>"><?php echo esc_html( get_the_modified_date( 'D, M jS, Y · g:i a' ) ); ?></time></a>
+			</div>
+
+		<?php endif; ?>
+
 		<?php if ( has_post_format() && ! is_page() ) : ?>
 			<div class="entry__meta">
 				<a href="<?php the_permalink(); ?>" class="entry__date u-url"><span class="entry__date-arrow">&rarr;</span><time class="dt-published" datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date( 'D, M jS, Y · g:i a' ); ?></time></a>
