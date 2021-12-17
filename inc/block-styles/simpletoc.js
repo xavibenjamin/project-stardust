eb7dacc3c79c5d7fb2efc9cb6634b5fd1f87eb26
Timothy B. Smith <tim@smithtimmytim.com>
2021-12-16 21:06:57 -0800

    add simpletoc styles

---
+++ inc/block-styles/simpletoc.js
@@ -0,0 +1,18 @@
+import { registerBlockStyle } from '@wordpress/blocks';
+
+function registerDriverStyles() {
+	registerBlockStyle(
+		'simpletoc/toc',
+		[
+			{
+				label: 'Default',
+				name: 'simpletoc-wrapper',
+				isDefault: true,
+			},
+		],
+	);
+}
+
+wp.domReady(() => {
+	registerDriverStyles();
+});
