b0edb85f9de36703b598a208864341c99d279d91
Tim Smith <tim@smithtimmytim.com>
2022-08-05 11:52:54 -0700

    add eslintrc and lint

--- includes/blocks/driver/edit.js
+++ includes/blocks/driver/edit.js
@@ -8,11 +8,11 @@ import { RichText, InnerBlocks, useBlockProps } from '@wordpress/block-editor';
  * Edit component.
  * See https://wordpress.org/gutenberg/handbook/designers-developers/developers/block-api/block-edit-save/#edit
  *
- * @param {Object}   props                        The block props.
- * @param {Object}   props.attributes             Block attributes.
- * @param {string}   props.attributes.customTitle Custom title to be displayed.
- * @param {string}   props.className              Class name for the block.
- * @param {Function} props.setAttributes          Sets the value for block attributes.
+ * @param {Object}   props                  The block props.
+ * @param {Object}   props.attributes       Block attributes.
+ * @param {string}   props.attributes.title Custom title to be displayed.
+ * @param {string}   props.className        Class name for the block.
+ * @param {Function} props.setAttributes    Sets the value for block attributes.
  * @return {Function} Render the edit screen
  */
 const DriverBlockEdit = ({
