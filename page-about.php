91f9e9101b6d24488d4b901e12e2a5b66237429f
Tim Smith <tim@smithtimmytim.com>
2022-08-12 16:27:33 -0700

    update markup of about template

--- page-about.php
+++ page-about.php
@@ -10,27 +10,28 @@ get_header();
 ?>
 
 <main tabindex="-1" id="main-content" class="site-content">
-	<div class="about-cover">
-		<?php the_post_thumbnail( 'full' ); ?>
-	</div>
 
-	<div class="about-body">
-		<div class="wrapper wrapper--large grid grid--about">
-			<div class="about-body__main">
-				<div class="entry__content hyphens">
-					<?php the_content(); ?>
-				</div>
-			</div>
-			<div class="about-body__sidebar">
-				<?php
-					get_template_part( 'template-parts/about/last-fm' );
-					get_template_part( 'template-parts/about/letterboxd' );
-				?>
-			</div>
+	<article class="grid grid--subgrid entry">
+
+		<div class="entry__featured-image alignfull">
+			<?php the_post_thumbnail( 'full' ); ?>
+			<?php if ( get_the_post_thumbnail_caption() ) : ?>
+				<figcaption class="entry__featured-image-caption"><?php the_post_thumbnail_caption(); ?></figcaption>
+			<?php endif; ?>
 		</div>
-	</div>
+
+		<div class="entry__content hyphens">
+			<?php the_content(); ?>
+		</div>
+
+		<div class="entry__aside entry__aside--about">
+			<?php
+				get_template_part( 'template-parts/about/last-fm' );
+				get_template_part( 'template-parts/about/letterboxd' );
+			?>
+		</div>
+
+	</article>
 </main>
 
-<?php
-get_footer();
-?>
+<?php get_footer(); ?>
